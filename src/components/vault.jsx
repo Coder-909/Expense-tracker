7e76819e2389fd51e96bf385c51c69e4106b1e6c
Ayush vashisth <ayushvashisth@Ayushs-MacBook-Air.local>
2024-11-05 19:43:53 +0530

    5 nov 2024, added some vault layout

--- src/components/vault.jsx
+++ src/components/vault.jsx
@@ -5,11 +5,21 @@ export default function Vault(){
     return (
         <div id="vault">
             <div id="left-side">
-                Total balance:
+                <h2>Total balance:</h2>
+                <p id="total-amount">1,53,098</p>
+                <div className="balance-change">
+                    <span className="balance-change-symbol green">^</span>
+                    <p className="balance-change-amount green">1500</p>
+                </div>
             </div>
             <div id ="right-side">
                 <div className="card">
-                    card
+                    <h2 className="name-of-storage">Bank of baroda</h2>
+                    <p className="specific-amount"></p>
+                    <div className="balance-change">
+                        <span className="balance-change-symbol green">^</span>
+                        <p className="balance-change-amount green">1500</p>
+                    </div>                
                 </div>
             </div>
         </div>
